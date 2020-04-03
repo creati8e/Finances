87da1158a725cb6d46fee75641bef11c40bb1e4a
Sergey Chuprin <gregamer@gmail.com>
2020-04-03 19:29:37 +0300

    Fix styles hierarchy; Make app fullscreen; Add simple dashboard layout

--- feature/feature-dashboard/src/main/kotlin/serg/chuprin/finances/feature/dashboard/presentation/view/DashboardFragment.kt
+++ feature/feature-dashboard/src/main/kotlin/serg/chuprin/finances/feature/dashboard/presentation/view/DashboardFragment.kt
@@ -1,9 +1,19 @@
 package serg.chuprin.finances.feature.dashboard.presentation.view
 
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 import serg.chuprin.finances.core.api.presentation.view.BaseFragment
 import serg.chuprin.finances.feature.dashboard.R
 
 /**
  * Created by Sergey Chuprin on 03.04.2020.
  */
-class DashboardFragment : BaseFragment(R.layout.fragment_dashboard)
+class DashboardFragment : BaseFragment(R.layout.fragment_dashboard) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        avatarImageView.clipToOutline = true
+    }
+
+}
