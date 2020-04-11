4ae2e0ea1604b5cf56f95165a3561a422ce917b0
Sergey Chuprin <gregamer@gmail.com>
2020-04-11 10:06:57 +0300

    Add page change animation

--- feature/feature-onboarding/src/main/kotlin/serg/chuprin/finances/feature/onboarding/presentation/common/view/OnboardingContainerFragment.kt
+++ feature/feature-onboarding/src/main/kotlin/serg/chuprin/finances/feature/onboarding/presentation/common/view/OnboardingContainerFragment.kt
@@ -1,8 +1,12 @@
 package serg.chuprin.finances.feature.onboarding.presentation.common.view
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.core.animation.doOnCancel
+import androidx.core.animation.doOnEnd
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import kotlinx.android.synthetic.main.fragment_onboarding_container.*
 import serg.chuprin.finances.core.api.domain.model.OnboardingStep
 import serg.chuprin.finances.core.api.presentation.model.viewmodel.extensions.component
@@ -13,6 +17,7 @@ import serg.chuprin.finances.feature.onboarding.R
 import serg.chuprin.finances.feature.onboarding.presentation.common.di.OnboardingFeatureComponent
 import serg.chuprin.finances.feature.onboarding.presentation.common.view.adapter.OnboardingPagerAdapter
 import javax.inject.Inject
+
 
 /**
  * Created by Sergey Chuprin on 06.04.2020.
@@ -40,7 +45,12 @@ class OnboardingContainerFragment : BaseFragment(R.layout.fragment_onboarding_co
 
     private fun handleNavigationEvent(onboardingStep: OnboardingStep) {
         val page = when (onboardingStep) {
-            OnboardingStep.ACCOUNT_SETUP -> 1
+            OnboardingStep.ACCOUNT_SETUP -> {
+                if (onboardingViewPager.beginFakeDrag()) {
+                    animatePageChange()
+                }
+                return
+            }
             OnboardingStep.CURRENCY_CHOICE -> 0
             OnboardingStep.COMPLETED -> {
                 navigation.navigateToDashboard(navController)
@@ -51,12 +61,33 @@ class OnboardingContainerFragment : BaseFragment(R.layout.fragment_onboarding_co
     }
 
     private fun setupViewPager() {
-        pagerAdapter =
-            OnboardingPagerAdapter(
-                this
-            )
+        pagerAdapter = OnboardingPagerAdapter(this)
         onboardingViewPager.adapter = pagerAdapter
         onboardingViewPager.isUserInputEnabled = false
+    }
+
+    private var oldDragPosition = 0
+
+    private fun animatePageChange() {
+        ValueAnimator
+            .ofInt(0, onboardingViewPager.width - 1)
+            .run {
+                doOnCancel {
+                    onboardingViewPager.endFakeDrag()
+                }
+                doOnEnd {
+                    onboardingViewPager.endFakeDrag()
+                }
+                addUpdateListener { animation ->
+                    val dragPosition = animation.animatedValue as Int
+                    val dragOffset: Int = dragPosition - oldDragPosition
+                    oldDragPosition = dragPosition
+                    onboardingViewPager.fakeDragBy(dragOffset * -1f)
+                }
+                duration = 400L
+                interpolator = FastOutSlowInInterpolator()
+                start()
+            }
     }
 
 }
