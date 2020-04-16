aaa51b06c47276e5be5b03b815b5575e6852c51f
Sergey Chuprin <gregamer@gmail.com>
2020-04-16 17:33:12 +0300

    Fix test

--- feature/feature-onboarding/src/main/kotlin/serg/chuprin/finances/feature/onboarding/domain/OnboardingMoneyAccountCreationParams.kt
+++ feature/feature-onboarding/src/main/kotlin/serg/chuprin/finances/feature/onboarding/domain/OnboardingMoneyAccountCreationParams.kt
@@ -5,7 +5,7 @@ import serg.chuprin.finances.core.api.domain.model.MoneyAccountBalance
 /**
  * Created by Sergey Chuprin on 13.04.2020.
  */
-class OnboardingMoneyAccountCreationParams(
+data class OnboardingMoneyAccountCreationParams(
     val accountName: String,
     val balance: MoneyAccountBalance
 )
