ba3a8d4b9969c80800792021d08b41bfdd303ab4
Sergey Chuprin <gregamer@gmail.com>
2020-04-30 18:40:57 +0300

    Make DSL markers internal

--- core/core-api/src/main/kotlin/serg/chuprin/finances/core/api/presentation/view/adapter/dsl/marker/RecyclerViewAdapterContextDslMarker.kt
+++ core/core-api/src/main/kotlin/serg/chuprin/finances/core/api/presentation/view/adapter/dsl/marker/RecyclerViewAdapterContextDslMarker.kt
@@ -4,4 +4,4 @@ package serg.chuprin.finances.core.api.presentation.view.adapter.dsl.marker
  * Created by Sergey Chuprin on 29.04.2020.
  */
 @DslMarker
-annotation class RecyclerViewAdapterContextDslMarker
+internal annotation class RecyclerViewAdapterContextDslMarker
