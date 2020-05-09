3503f21cb0acd1e002d77414cc122afb7bc91305
Sergey Chuprin <gregamer@gmail.com>
2020-05-09 11:27:51 +0300

    Create custom class for toolbar layout and use material shape drawable

---
+++ feature/feature-money-account-details/src/main/java/serg/chuprin/finances/feature/moneyaccount/details/presentation/view/widget/MoneyAccountDetailsToolbarLayout.kt
@@ -0,0 +1,34 @@
+package serg.chuprin.finances.feature.moneyaccount.details.presentation.view.widget
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import com.google.android.material.shape.MaterialShapeDrawable
+import serg.chuprin.finances.core.api.presentation.view.extensions.getAttributeColor
+
+/**
+ * Created by Sergey Chuprin on 09.05.2020.
+ */
+class MoneyAccountDetailsToolbarLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val materialShapeDrawable = MaterialShapeDrawable.createWithElevationOverlay(context)
+
+    init {
+        materialShapeDrawable.fillColor = ColorStateList.valueOf(
+            context.getAttributeColor(android.R.attr.colorBackground)
+        )
+        background = materialShapeDrawable
+    }
+
+    override fun setElevation(elevation: Float) {
+        super.setElevation(elevation)
+        materialShapeDrawable.elevation = elevation
+    }
+
+
+}
