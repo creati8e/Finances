02b012137c6833e6de4fca1a69c0e885a8b42aae
Sergey Chuprin <gregamer@gmail.com>
2020-05-11 19:08:57 +0300

    Add module and fragment for transactions report feature

---
+++ feature/feature-transactions/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlin-android-extensions")
+    id("androidx.navigation.safeargs")
+}
+
+dependencies {
+    implementation(project(":core:core-api"))
+    implementation(project(":injector"))
+
+    implementation(Libraries.KOTLIN)
+    implementation(Libraries.Coroutines.CORE)
+    implementation(Libraries.Coroutines.ANDROID)
+
+    // region UI.
+
+    implementation(Libraries.COIL)
+
+    // Navigation.
+    implementationAll(Libraries.Android.Navigation.DEPENDENCIES)
+
+    // Android.
+    implementation(Libraries.Android.CORE)
+    implementation(Libraries.Android.DESIGN)
+    implementation(Libraries.Android.FRAGMENT)
+    implementation(Libraries.Android.APPCOMPAT)
+    implementation(Libraries.Android.CONSTRAINT_LAYOUT)
+
+    implementationAll(Libraries.Adapter.DEPENDENCIES)
+
+    // endregion
+
+    // region DI.
+
+    kapt(Libraries.Dagger.COMPILER)
+    implementationAll(Libraries.Dagger.DEPENDENCIES)
+
+    // endregion
+
+    // Architecture components.
+    implementationAll(Libraries.Android.Lifecycle.DEPENDENCIES)
+
+}
---
+++ feature/feature-transactions/src/main/kotlin/serg/chuprin/finances/feature/transactions/presentation/view/TransactionsReportFragment.kt
@@ -0,0 +1,9 @@
+package serg.chuprin.finances.feature.transactions.presentation.view
+
+import serg.chuprin.finances.core.api.presentation.view.BaseFragment
+import serg.chuprin.finances.feature.transactions.R
+
+/**
+ * Created by Sergey Chuprin on 11.05.2020.
+ */
+class TransactionsReportFragment : BaseFragment(R.layout.fragment_transactions_report)
