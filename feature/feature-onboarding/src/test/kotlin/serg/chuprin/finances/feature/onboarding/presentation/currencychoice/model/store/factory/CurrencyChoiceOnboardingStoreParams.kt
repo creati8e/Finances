8a4628f36d968f2192c29fdac5f037b171bfdae7
Sergey Chuprin <gregamer@gmail.com>
2020-12-09 21:29:21 +0300

    Delete old TestStateStore

--- feature/feature-onboarding/src/test/kotlin/serg/chuprin/finances/feature/onboarding/presentation/currencychoice/model/store/factory/CurrencyChoiceOnboardingStoreParams.kt
+++ feature/feature-onboarding/src/test/kotlin/serg/chuprin/finances/feature/onboarding/presentation/currencychoice/model/store/factory/CurrencyChoiceOnboardingStoreParams.kt
@@ -1,15 +1,17 @@
 package serg.chuprin.finances.feature.onboarding.presentation.currencychoice.model.store.factory
 
 import serg.chuprin.finances.core.api.domain.repository.CurrencyRepository
-import serg.chuprin.finances.core.test.utils.TestStateStore
+import serg.chuprin.finances.core.test.store.TestStore
 import serg.chuprin.finances.feature.onboarding.domain.usecase.CompleteCurrencyChoiceOnboardingUseCase
-import serg.chuprin.finances.feature.onboarding.presentation.currencychoice.model.store.*
+import serg.chuprin.finances.feature.onboarding.presentation.currencychoice.model.store.CurrencyChoiceOnboardingEvent
+import serg.chuprin.finances.feature.onboarding.presentation.currencychoice.model.store.CurrencyChoiceOnboardingIntent
+import serg.chuprin.finances.feature.onboarding.presentation.currencychoice.model.store.CurrencyChoiceOnboardingState
 
 /**
  * Created by Sergey Chuprin on 14.04.2020.
  */
 class CurrencyChoiceOnboardingStoreParams(
-    val store: TestStateStore<CurrencyChoiceOnboardingIntent, CurrencyChoiceOnboardingEffect, CurrencyChoiceOnboardingAction, CurrencyChoiceOnboardingState, CurrencyChoiceOnboardingEvent>,
+    val store: TestStore<CurrencyChoiceOnboardingIntent, CurrencyChoiceOnboardingState, CurrencyChoiceOnboardingEvent>,
     val currencyRepository: CurrencyRepository,
     val completeCurrencyChoiceOnboardingUseCase: CompleteCurrencyChoiceOnboardingUseCase
 )
