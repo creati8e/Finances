70eb1276f1ae353804d49dccbe1e0c179ae1d683
Sergey Chuprin <gregamer@gmail.com>
2020-12-22 13:38:08 +0300

    Delete TransactionReportDataPeriodRepository

--- feature/feature-transactions/src/main/kotlin/serg/chuprin/finances/feature/transactions/di/TransactionsReportModule.kt
+++ feature/feature-transactions/src/main/kotlin/serg/chuprin/finances/feature/transactions/di/TransactionsReportModule.kt
@@ -3,9 +3,7 @@ package serg.chuprin.finances.feature.transactions.di
 import dagger.Binds
 import dagger.Module
 import serg.chuprin.finances.core.api.di.scopes.ScreenScope
-import serg.chuprin.finances.feature.transactions.data.repository.impl.TransactionReportDataPeriodRepositoryImpl
 import serg.chuprin.finances.feature.transactions.data.repository.impl.TransactionReportFilterRepositoryImpl
-import serg.chuprin.finances.feature.transactions.domain.repository.TransactionReportDataPeriodRepository
 import serg.chuprin.finances.feature.transactions.domain.repository.TransactionReportFilterRepository
 
 /**
@@ -18,10 +16,5 @@ interface TransactionsReportModule {
     fun bindsFilterRepository(
         impl: TransactionReportFilterRepositoryImpl
     ): TransactionReportFilterRepository
-
-    @[Binds ScreenScope]
-    fun bindsTransactionReportDataPeriodRepository(
-        impl: TransactionReportDataPeriodRepositoryImpl
-    ): TransactionReportDataPeriodRepository
 
 }
