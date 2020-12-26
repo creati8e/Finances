eff271c4549ae82f4b22bffdb07112df4946346b
Sergey Chuprin <gregamer@gmail.com>
2020-12-26 10:16:18 +0300

    Fix color of balance transaction amount text

--- core/core-api/src/main/kotlin/serg/chuprin/finances/core/api/presentation/view/adapter/renderer/TransactionCellRenderer.kt
+++ core/core-api/src/main/kotlin/serg/chuprin/finances/core/api/presentation/view/adapter/renderer/TransactionCellRenderer.kt
@@ -5,7 +5,10 @@ import kotlinx.android.synthetic.main.cell_transaction.*
 import serg.chuprin.adapter.ContainerHolder
 import serg.chuprin.adapter.ContainerRenderer
 import serg.chuprin.finances.core.api.R
+import serg.chuprin.finances.core.api.domain.model.transaction.TransactionType
 import serg.chuprin.finances.core.api.presentation.model.cells.TransactionCell
+import serg.chuprin.finances.core.api.presentation.view.extensions.getColorInt
+import serg.chuprin.finances.core.api.presentation.view.extensions.getPrimaryTextColor
 import serg.chuprin.finances.core.api.presentation.view.extensions.makeVisibleOrGone
 
 /**
@@ -18,12 +21,27 @@ class TransactionCellRenderer : ContainerRenderer<TransactionCell>() {
     override fun bindView(holder: ContainerHolder, model: TransactionCell) {
         with(holder) {
             timeTextView.text = model.time
-            amountTextView.text = model.amount
-            amountTextView.isActivated = model.isIncome
             subcategoryTextView.text = model.subcategoryName
             parentCategoryTextView.text = model.parentCategoryName
             transactionColorDot.imageTintList = ColorStateList.valueOf(model.color)
             subcategoryTextView.makeVisibleOrGone(model.subcategoryName.isNotEmpty())
+
+            with(amountTextView) {
+                text = model.amount
+
+                when (model.transaction.type) {
+                    TransactionType.PLAIN -> {
+                        if (model.transaction.isIncome) {
+                            setTextColor(context.getColorInt(R.color.colorGreen))
+                        } else {
+                            setTextColor(context.getColorInt(R.color.colorRed))
+                        }
+                    }
+                    TransactionType.BALANCE -> {
+                        setTextColor(context.getPrimaryTextColor())
+                    }
+                }
+            }
         }
     }
 }
