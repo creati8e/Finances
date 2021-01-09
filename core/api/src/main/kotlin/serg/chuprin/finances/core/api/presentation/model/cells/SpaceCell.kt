39610c89469b8ce1bdade0d66ff6730c08952c9c
Sergey Chuprin <gregamer@gmail.com>
2021-01-09 22:04:22 +0300

    Delete unused methods, add 'data' modifier to SpaceCell

--- core/api/src/main/kotlin/serg/chuprin/finances/core/api/presentation/model/cells/SpaceCell.kt
+++ core/api/src/main/kotlin/serg/chuprin/finances/core/api/presentation/model/cells/SpaceCell.kt
@@ -3,6 +3,6 @@ package serg.chuprin.finances.core.api.presentation.model.cells
 /**
  * Created by Sergey Chuprin on 25.12.2020.
  */
-class SpaceCell(
+data class SpaceCell(
     val sizeDp: Int
 ) : BaseCell
