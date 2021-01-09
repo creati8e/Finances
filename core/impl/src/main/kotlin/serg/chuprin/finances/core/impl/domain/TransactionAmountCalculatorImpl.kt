69a49dfe90a18e0594d21c7649f4541b87eb32f8
Sergey Chuprin <gregamer@gmail.com>
2021-01-09 23:10:09 +0300

    Add forgotten logic for 'isAbsoluteAmount' in amount calculator

--- core/impl/src/main/kotlin/serg/chuprin/finances/core/impl/domain/TransactionAmountCalculatorImpl.kt
+++ core/impl/src/main/kotlin/serg/chuprin/finances/core/impl/domain/TransactionAmountCalculatorImpl.kt
@@ -15,7 +15,14 @@ internal class TransactionAmountCalculatorImpl @Inject constructor() : Transacti
         isAbsoluteAmount: Boolean
     ): BigDecimal {
         // TODO: Implement multi-currency logic.
-        return transactions.fold(BigDecimal.ZERO, { acc, transaction -> acc + transaction.amount })
+        return transactions.fold(BigDecimal.ZERO, { acc, transaction ->
+            val amount = if (isAbsoluteAmount) {
+                transaction.amount.abs()
+            } else {
+                transaction.amount
+            }
+            acc + amount
+        })
     }
 
 }
